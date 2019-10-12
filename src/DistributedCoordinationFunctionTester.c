661a1e448f62b6a9472f934087fe0983c8f053c8
Lena Voytek <dvoytek@email.arizona.edu>
2019-10-12 14:53:38 -0700

    Tester: Created test files

---
+++ src/DistributedCoordinationFunctionTester.c
@@ -0,0 +1,18 @@
+#include "DistributedCoordinationFunctionTester.h"
+
+void testPoisson()
+{
+	int * items = generatePoissonDelayTimes(50, 10);
+
+	printf("%d", items[0]);
+	
+	for(int i = 1; i < 500; i++) {
+		printf(", %d", items[i]);
+	}
+}
+
+int main()
+{
+	testPoisson();
+	return 0;
+}
---
+++ src/DistributedCoordinationFunctionTester.h
@@ -0,0 +1,9 @@
+#ifndef DISTRIBUTEDCOORDINATIONFUNCTIONTESTER_H
+#define DISTRIBUTEDCOORDINATIONFUNCTIONTESTER_H
+
+#include <stdio.h>
+#include "DistributedCoordinationFunction.h"
+
+void testPoisson();
+
+#endif
