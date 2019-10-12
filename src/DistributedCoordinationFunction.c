12cec3c62491d308eccf2ffbc1ac2239e0032a10
Lena Voytek <dvoytek@email.arizona.edu>
2019-10-12 15:41:12 -0700

    Working on DCF

--- src/DistributedCoordinationFunction.c
+++ src/DistributedCoordinationFunction.c
@@ -5,7 +5,7 @@ int * generatePoissonDelayTimes(int lambda, int time)
 	int * delayTimes = (int *) calloc(sizeof(int), lambda*time);
 
 	for(int i = 0; i < lambda*time; i++) {
-		delayTimes[i] = -1.0 / lambda * log(1 - (rand() & 1));
+		delayTimes[i] = -1.0 / lambda * log(1 - (rand()%100 / 100.0));
 	}
 
 	return delayTimes;
