31138fffe58541b96ea1b0071df9226c2d459fc3
mitchdz <mitch_dz@hotmail.com>
2019-10-13 21:04:21 -0700

    [WIP] A1sim: initial structure implementation
    Signed-off-by: mitchdz <mitch_dz@hotmail.com>

--- src/A1sim.c
+++ src/A1sim.c
@@ -3,26 +3,79 @@
 // Concurrent Communications using CSMA/CA
 A1simulation();
 
-int main() {
+int currentlyOccupied = FALSE;
+
+
+int main() 
+{
 	A1simulation();
 }
 
-void A1simulation() {
-	// Random backoff time
+void A1simulation() 
+{
+	int *AbackoffTimes;
+	int *CbackoffTimes;
+
+	int lambdaA = {50, 100, 200, 300, 500, 100, 200, 400, 600, 1000};
+	int lambdaC = {50, 100, 200, 300, 500, 50, 100, 200, 300, 500};
+
+	for (int i = 0; i < 10; i++) {
+		AbackoffTimes = generatePoissonDelayTimes(lambdaA[i], simulation_time_S, 100000);
+		CbackoffTimes = generatePoissonDelayTimes(lambdaC[i], simulation_time_S, 100000);
+		A1_sim_run(AbackoffTimes, BbackoffTimes);
+		free(AbackoffTimes);
+		free(CbackoffTimes);
+	}
+}
+
+void A1_sim_run(int *Abackoff, int *Cbackoff)
+{
+	slot_t curr_slot = {FALSE};
+	slot_t next_slot = {FALSE};
+
+	int *currAback Abackoff;
+	int *currCback Cbackoff;
+
+	//NAV = DIFS + backoff + Tansmission time + SIFS + ACK
+	int NAV_without_backoff = \
+		DIFS_duration_us/slot_duration_us + \
+		slots_from_bytes(data_frame_size_bytes) + \
+		SIFS_duration_us/slot_duration_us + \
+		slots_from_bytes(ACK_RTS_CTS_size_bytes);
 
 
-	// if busy, monitors until idle. 
-	//		If idle, Tx decrements with every idle slot.
-	// 		if busy, Tx freezes its backoff counter.
-	//		when counter reaches zero, Tx transmits its frame.
+	for(int i = 0; i < total_slots; i++) {
+
+		if (curr_slot.occupied == FALSE) {
+			if (*currAback == 0)
+				startTransmission("A");
+			if (*currCback == 0)
+				startTransmission("C");
+		}
+
+		// if busy, monitors until idle. 
+		//		If idle, Tx decrements with every idle slot.
+		// 		if busy, Tx freezes its backoff counter.
+		//		when counter reaches zero, Tx transmits its frame.
 
 
-	// if frame is successfully received by Rx, Rx replies with 
-	// 		ACK after SIFS time. 
+		// if frame is successfully received by Rx, Rx replies with 
+		// 		ACK after SIFS time. 
 
 
-	// if collision occurs, stations that collided doubles their 
-	// 		contention window and repeats the backoff process
-	//		after k collisions, backoff value is selected from
-	// 		[0, 2^k*CW_0 - 1]. CW can not exceed CWmax
+		// if collision occurs, stations that collided doubles their 
+		// 		contention window and repeats the backoff process
+		//		after k collisions, backoff value is selected from
+		// 		[0, 2^k*CW_0 - 1]. CW can not exceed CWmax
+	}
+}
+
+void startTransmission() 
+{
+	currentlyOccupied = TRUE;
+}
+
+void stopTransmission() 
+{
+	currentlyOccupied = FALSE;
 }
