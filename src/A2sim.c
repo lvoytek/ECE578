631186c16e9a29e6d2eba50b5938b5c9b6690ecf
Lena Voytek <dvoytek@email.arizona.edu>
2019-10-21 15:43:13 -0700

    A2 setup

--- src/A2sim.c
+++ src/A2sim.c
@@ -1,15 +1,67 @@
 #include "libDCF.h"
 
-// Concurrent Communications using CSMA/CA with virtual carrier 
-// sensing enabled
+// Concurrent Communications using CSMA/CA with virtual carrier
 void A2simulation();
+void A2_sim_run(node *A, node *C);
+
+const char *outputFileName = "A2simout.csv";
+
 
 int main()
 {
-	A2simulation();
+    A2simulation();
+    return 0;
 }
 
 void A2simulation()
+{
+    node A;
+    node C;
+
+    int lambdaA[] = {50, 100, 200, 300, 500, 100, 200, 400, 600, 1000};
+    int lambdaC[] = {50, 100, 200, 300, 500, 50, 100, 200, 300, 500};
+
+    int Acollisions[10] = {};
+    int Asuccesses[10] = {};
+    int Csuccesses[10] = {};
+    int Ccollisions[10] = {};
+    float fairnessIndex[10] = {};
+
+    printf("%sA2sim%s\n", BRIGHTMAGENTA, NONE);
+    for (int i = 0; i < 10; i++) {
+        initializeNode(&A);
+        A.sendDelayTimes = generatePoissonDelayTimes(lambdaA[i], SIMULATION_TIME_S, SLOTS_PER_SECOND);
+
+        initializeNode(&C);
+        C.sendDelayTimes = generatePoissonDelayTimes(lambdaC[i], SIMULATION_TIME_S, SLOTS_PER_SECOND);
+
+        A2_sim_run(&A, &C);
+
+        Acollisions[i] = A.totalCollisions;
+        Asuccesses[i] = A.totalSuccesses;
+        Ccollisions[i] = C.totalCollisions;
+        Csuccesses[i] = C.totalSuccesses;
+        fairnessIndex[i] = ((float)(A.slotsOccupied))/C.slotsOccupied;
+
+        printf("%sA2sim: lambdaA: %d lambdaC: %d %s\n", YELLOW, lambdaA[i], lambdaC[i], NONE);
+        printf("\tA successes: %d\n", A.totalSuccesses);
+        printf("\tA collisions: %d\n", A.totalCollisions);
+        printf("\tC successes: %d\n", C.totalSuccesses);
+        printf("\tC collisions: %d\n\n", C.totalCollisions);
+
+        printf("\tTotal Data Transferred: %d\n", (A.totalSuccesses + C.totalSuccesses) * DATA_FRAME_SIZE_BYTES);
+        printf("\tTotal Collisions: %d\n", A.totalCollisions + C.totalCollisions);
+        printf("\tSlots occupied by A:%d\n", A.slotsOccupied);
+        printf("\tSlots occupied by C:%d\n", C.slotsOccupied);
+        printf("\tFairness index: %f\n\n", fairnessIndex[i]);
+        free(A.sendDelayTimes);
+        free(C.sendDelayTimes);
+    }
+
+    sendToCSV(outputFileName, lambdaA, lambdaC, Asuccesses, Csuccesses, Acollisions, Ccollisions, fairnessIndex);
+}
+
+void A2_sim_run(node * A, node * C)
 {
 	//RTS and CTS exchanged
 
