d0fc6f61bc42611bc6a0a0adc270d0cad4d03510
mitchdz <mitch_dz@hotmail.com>
2019-10-21 15:39:26 -0700

    A1sim: change spaces to tabs

--- src/A1sim.c
+++ src/A1sim.c
@@ -21,15 +21,15 @@ void A1simulation()
 	int lambdaA[] = {50, 100, 200, 300, 500, 100, 200, 400, 600, 1000};
 	int lambdaC[] = {50, 100, 200, 300, 500, 50, 100, 200, 300, 500};
 
-    int Acollisions[10] = {};
-    int Asuccesses[10] = {};
-    int Csuccesses[10] = {};
-    int Ccollisions[10] = {};
-    float fairnessIndex[10] = {};
+	int Acollisions[10] = {};
+	int Asuccesses[10] = {};
+	int Csuccesses[10] = {};
+	int Ccollisions[10] = {};
+	float fairnessIndex[10] = {};
 
 	printf("%sA1sim%s\n", BRIGHTMAGENTA, NONE);
 	for (int i = 0; i < 10; i++) {
-	    initializeNode(&A);
+		initializeNode(&A);
 		A.sendDelayTimes = generatePoissonDelayTimes(lambdaA[i], SIMULATION_TIME_S, SLOTS_PER_SECOND);
 
 		initializeNode(&C);
@@ -37,11 +37,11 @@ void A1simulation()
 
 		A1_sim_run(&A, &C);
 
-        Acollisions[i] = A.totalCollisions;
-        Asuccesses[i] = A.totalSuccesses;
-        Ccollisions[i] = C.totalCollisions;
-        Csuccesses[i] = C.totalSuccesses;
-        fairnessIndex[i] = ((float)(A.slotsOccupied))/C.slotsOccupied;
+        	Acollisions[i] = A.totalCollisions;
+        	Asuccesses[i] = A.totalSuccesses;
+        	Ccollisions[i] = C.totalCollisions;
+        	Csuccesses[i] = C.totalSuccesses;
+        	fairnessIndex[i] = ((float)(A.slotsOccupied))/C.slotsOccupied;
 
 		printf("%sA1sim: lambdaA: %d lambdaC: %d %s\n", YELLOW, lambdaA[i], lambdaC[i], NONE);
 		printf("\tA successes: %d\n", A.totalSuccesses);
@@ -50,10 +50,10 @@ void A1simulation()
 		printf("\tC collisions: %d\n\n", C.totalCollisions);
 
 		printf("\tTotal Data Transferred: %d\n", (A.totalSuccesses + C.totalSuccesses) * DATA_FRAME_SIZE_BYTES);
-        printf("\tTotal Collisions: %d\n", A.totalCollisions + C.totalCollisions);
-        printf("\tSlots occupied by A:%d\n", A.slotsOccupied);
-        printf("\tSlots occupied by C:%d\n", C.slotsOccupied);
-        printf("\tFairness index: %f\n\n", fairnessIndex[i]);
+        	printf("\tTotal Collisions: %d\n", A.totalCollisions + C.totalCollisions);
+        	printf("\tSlots occupied by A:%d\n", A.slotsOccupied);
+        	printf("\tSlots occupied by C:%d\n", C.slotsOccupied);
+        	printf("\tFairness index: %f\n\n", fairnessIndex[i]);
 		free(A.sendDelayTimes);
 		free(C.sendDelayTimes);
 	}
