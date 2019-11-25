c9b765a2f8e482fdc2b047671ed5329b0ed481a2
Lena Voytek <dvoytek@email.arizona.edu>
2019-11-25 14:37:20 -0700

    Added height labels to node degree histogram

--- project2_Internet_Topology/src/ASTopology.py
+++ project2_Internet_Topology/src/ASTopology.py
@@ -158,14 +158,19 @@ class ASTopology:
 			else:
 				bins[5] += 1
 
-		bin_names = ["1", "2-5", "5-100", "100-200", "200-1000", ">1000"]
+		bin_names = ["1", "2-5", "6-100", "101-200", "201-1000", ">1000"]
 
 		fig1, ax1 = plt.subplots()
-		ax1.bar([0, 1, 2, 3, 4, 5], bins)
+		rects = ax1.bar([0, 1, 2, 3, 4, 5], bins)
 		plt.xticks([0, 1, 2, 3, 4, 5], bin_names)
 		plt.title("AS Node Degree Distribution")
 		plt.xlabel("# of Distinct Links")
 		plt.ylabel("Number of ASes")
+
+		for rect in rects:
+			height = rect.get_height()
+			ax1.text(rect.get_x() + rect.get_width() / 2., height + 10, '%d' % int(height), ha='center', va='bottom')
+
 		plt.savefig('output/node_degree.png', dpi=300, edgecolor='w', format='png', pad_inches=0.1)
 		plt.show()
 
